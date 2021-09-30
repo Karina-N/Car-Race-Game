a2d54753d05b9a48c2f15817784601797e7a1e7b
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-09-30 18:52:16 +0300

    add moving obstacles

--- js/game.js
+++ js/game.js
@@ -2,65 +2,108 @@ class Game {
   constructor() {
     this.currentTime = 0;
     this.car = null;
+    this.obstaclesArray = [];
   }
 
   startGame() {
     this.car = new Car();
     this.car.create();
     this.addEventListeners();
+
+    // OPTION 1
+    // setInterval(() => {
+    //   const newObstacle = new Obstacle();
+    //   this.obstaclesArray.push(newObstacle);
+    //   newObstacle.create();
+    //   newObstacle.draw();
+    // }, 6000);
+
+    // setInterval(() => {
+    //   //@todo: instead of updating one, we need to update all the obstacles we have.
+    //   this.obstaclesArray.forEach((elm) => {
+    //     elm.moveDown();
+    //     elm.draw();
+    //   });
+    // }, 1000);
+
+    // OPTION 2
+    setInterval(() => {
+      // update timer
+      this.currentTime++;
+
+      // update positions
+      this.obstaclesArray.forEach((elm) => {
+        elm.moveDown();
+        elm.draw();
+      });
+
+      // create new obstacles
+      if (this.currentTime % 8 === 0) {
+        const newObstacle = new Obstacle();
+        this.obstaclesArray.push(newObstacle);
+        newObstacle.create();
+      }
+    }, 200);
   }
+
   addEventListeners() {
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowLeft") {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowLeft") {
         this.car.moveLeft();
-        this.car.draw();
-      } else if (e.key === "ArrowRight") {
+        this.car.draw(); //@todo
+      } else if (event.key === "ArrowRight") {
         this.car.moveRight();
-        this.car.draw();
+        this.car.draw(); //@todo
       }
     });
   }
 }
 
-class Car {
+class Thing {
   constructor() {
-    this.x = 50;
-    this.y = 0;
-    this.width = 8;
-    this.height = 16;
-    this.carDiv = document.createElement("div");
+    this.domElm = null;
   }
+  create() {
+    this.domElm = document.createElement("div");
+    this.domElm.className = this.className;
+    const gameElm = document.getElementById("game");
+    gameElm.appendChild(this.domElm);
+  }
+  draw() {
+    this.domElm.style.width = this.width + "%";
+    this.domElm.style.height = this.height + "%";
+    this.domElm.style.left = this.x + "%";
+    this.domElm.style.top = this.y + "%";
+  }
+}
 
+class Car extends Thing {
+  constructor() {
+    super();
+    this.x = 50;
+    this.y = 100;
+    this.width = 10;
+    this.height = 20;
+    this.className = "car";
+  }
   moveLeft() {
-    if (this.x > 0) {
-      this.x--;
-      this.draw();
-    }
+    this.x--;
   }
   moveRight() {
-    if (this.x <= 99 - this.width) {
-      this.x++;
-      this.draw();
-    }
+    this.x++;
   }
+}
 
-  create() {
-    this.carDiv.className = "car";
-
-    this.carDiv.style.width = this.width + "%";
-    this.carDiv.style.height = this.height + "%";
-    this.carDiv.style.left = this.x + "%";
-    this.carDiv.style.bottom = this.y + "%";
-
-    //append to DOM
-    const gameElm = document.getElementById("game");
-    gameElm.appendChild(this.carDiv);
+class Obstacle extends Thing {
+  constructor() {
+    super();
+    this.x = 50;
+    this.y = 0;
+    this.width = 20;
+    this.height = 5;
+    this.className = "obstacle";
   }
-
-  draw() {
-    this.carDiv.style.width = this.width + "%";
-    this.carDiv.style.height = this.height + "%";
-    this.carDiv.style.left = this.x + "%";
-    this.carDiv.style.bottom = this.y + "%";
+  moveDown() {
+    this.y = this.y + 5;
   }
 }
